1fce8b608a7a05a43948c89165623f3379a6eb32
Студент 31101 <u31101@ap47.local>
2022-09-24 10:35:41 +1000

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Rassokhin_PR1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void K1(object sender, RoutedEventArgs e)
+        {
+            t1.Background = Brushes.White;
+        }
+        private void K2(object sender, RoutedEventArgs e)
+        {
+            t2.Background = Brushes.White;
+        }
+        private void K3(object sender, RoutedEventArgs e)
+        {
+            t3.Background = Brushes.White;
+        }
+        private void K4(object sender, RoutedEventArgs e)
+        {
+            t4.Background = Brushes.White;
+        }
+    }
+}
